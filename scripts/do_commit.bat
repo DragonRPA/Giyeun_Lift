dc0f1672d9f6c54daced2fc16cc74e191da8f277
DragonRPA <77.victor.lee@gmail.com>
2026-09-06 20:18:56 +0900

    v1.9.4.Build.3: dispatch4 left-pane only scroll, right-pane always fixed 2026-09-06

--- scripts/do_commit.bat
+++ scripts/do_commit.bat
@@ -1,4 +1,4 @@
 @echo off
 git add -A
-git commit -m "v1.9.4.Build.2: remove APK card from sales home, rename to geunmujung 2026-09-06"
+git commit -m "v1.9.4.Build.3: dispatch4 left-pane only scroll, right-pane always fixed 2026-09-06"
 git push origin main

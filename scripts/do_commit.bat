7ec82a978a7d348d0234084a204f01673f477860
DragonRPA <77.victor.lee@gmail.com>
2026-09-06 19:49:07 +0900

    v1.9.3.Build.5: fix dispatch4 block clipping 2026-09-06

--- scripts/do_commit.bat
+++ scripts/do_commit.bat
@@ -1,4 +1,4 @@
 @echo off
 git add -A
-git commit -m "v1.9.3.Build.3+Build.4: UI improvement and APK triple-signing no-cache deployment 2026-09-06"
+git commit -m "v1.9.3.Build.5: fix dispatch4 block clipping 2026-09-06"
 git push origin main
